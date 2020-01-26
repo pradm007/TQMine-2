8cbe5882e698729ae7d66a3e867f7ae0d1571578
Pradeep Mahato <pradeepmahato007@gmail.com>
2020-01-26 13:06:50 -0800

    C++ scaffolding completed

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @author Pradeep Mahato
+ * @email pradeepmahato007@gmail.com
+ * @create date 2020-01-26 12:35:52
+ * @modify date 2020-01-26 12:35:52
+ */
+
+#include "../lib/commonHeader.h"
+
+int main(int argc, char *argv[]) {
+	cout << "Hello Easy C++ project!" << endl;
+	return 0;
+}
