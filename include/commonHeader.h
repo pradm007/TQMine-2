be664edeb27f241c331561e6bca5d69497bd875c
Pradeep Mahato <pradeepmahato007@gmail.com>
2020-01-26 16:25:25 -0800

    Rearranging header file

---
+++ include/commonHeader.h
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define maxOf(a, b) ((a) > (b) ? (a) : (b))
+#define minOf(a, b) ((a) < (b) ? (a) : (b))
+typedef long long unsigned lln;
+typedef long double lld;
